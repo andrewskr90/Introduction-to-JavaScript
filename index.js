fe7ff51ff6d7cb1e52039d9ee2025292251076b2
Kyle Andrews <andrewskr90@gmail.com>
2021-06-08 21:24:43 -0700

    completed assignment

--- index.js
+++ index.js
@@ -173,35 +173,41 @@ let computer= Math.random();
   } else if (computer >=0.666 && computer <= 1){
     computer = 'Scissors';
   }
+  function game(user,computer){
 
-function game(user, computer){
-  if(user='Rock'){
-    var choice=1;
-  } else if(user='Paper'){
-    choice=2;
-  }else if(user='Scissors'){
-    choice=3;
-  }
-  if(computer === 'Rock' && choice === 1){
+  if(computer === 'rock' && user === 'rock'){
     return("it's a tie");
-  } else if(computer === 'Rock' && choice === 2){
-    return('you lose!');
-  } else if(computer === 'Rock' && choice === 3){
+  } else if(computer === 'rock' && user === 'paper'){
     return('you win!');
-  } else if(computer === 'Paper' && choice === 1){
+  } else if(computer === 'rock' && user === 'scissors'){
     return('you lose!');
-  } else if(computer === 'Paper' && choice === 2){
-    return("it's a tie");
-  }else if(computer === 'Paper' && choice === 3){
-    return('you win!');
-  } else if(computer === 'Scissors' && choice === 1){
-    return('you win!');
-  }else if(computer === 'Scissors' && choice === 2){
+  } else if(computer === 'paper' && user === 'rock'){
     return('you lose!');
-  } else if(computer === 'Scissors' && choice === 3){
+  } else if(computer === 'paper' && user === 'paper'){
+    return(`it's a tie`);
+  }else if(computer === 'paper' && user === 'scissors'){
+    return('you win!');
+  } else if(computer === 'scissors' && user === 'rock'){
+    return('you win!');
+  }else if(computer === 'scissors' && user === 'paper'){
+    return('you lose!');
+  } else if(computer === 'scissors' && user === 'scissors'){
     return("it's a tie");
   }
-}console.log('task 4', game('Scissors',computer));
+}
+
+// function game(user, computer){
+//   if(user===computer){
+//     return("it's a tie");
+//   }else if(user === "rock" && computer === "scissors"){
+//     return("you win!");
+//   }else if(user === "paper" && computer === "rock"){
+//     return("you win!");
+//   }else if(user=== "scissors" && computer === "paper"){
+//     return("you win!");
+//   }else return("you lose!");
+//   }
+// game('Scissors',computer);
   
   
 
@@ -282,9 +288,20 @@ Using the grade function below do the following:
    below should return 'you got an F'
 */
   
-function grade(/*Your Code here */){
-  /*Your Code here */
+function grade(score){
+  if(score>=90 && score<=100){
+    return('you got an A');
+  } else if(score<=89 && score>=80){
+    return('you got a B');
+  } else if(score<=79 && score>=70){
+    return('you got a C');
+  } else if(score<=69 && score>=60){
+    return('you got a D');
+  } else if(score<60){
+    return('you got an F');
   }
+}
+grade(90);
   
   
 
